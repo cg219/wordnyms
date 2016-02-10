ecd3e254c10655c3b447e03b753d576b729c383d
Clemente Gomez <zomegpro@gmail.com>
2016-02-10 10:19:02 -0500

    Update Mongo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var routes = require("./app/routes")(express.Router());
 var nymAPI = require("./app/wordAPI")(express.Router(), app);
 var host = process.env.NODE_ENV == "production" ? "0.0.0.0" : "localhost";
-var port = process.env.PORT || 5000;
+var port = process.env.NODE_ENV == "production" ? process.env.PORT : 5000;
 
 app.set("gameLevel", 2);
 
