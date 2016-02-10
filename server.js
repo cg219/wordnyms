b83fbe4af27afcec2f58456ed4c9d540fba5c1e5
Clemente Gomez <zomegpro@gmail.com>
2016-02-10 11:47:43 -0500

    Update Host/Port for Heroku

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var bodyParser = require("body-parser");
 var app = express();
 var routes = require("./app/routes")(express.Router());
 var nymAPI = require("./app/wordAPI")(express.Router(), app);
-var host = process.env.NODE_ENV == "production" ? "0.0.0.0" : "localhost";
 var port = process.env.NODE_ENV == "production" ? process.env.PORT : 5000;
 
 app.set("gameLevel", 2);
@@ -12,6 +11,6 @@ app.use(express.static(__dirname + "/public"));
 app.use("/api/nyms", nymAPI);
 app.use("/", routes);
 
-app.listen(port, host, function(){
+app.listen(port, function(){
 	console.log("Running");
 });
