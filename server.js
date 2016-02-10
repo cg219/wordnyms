d378433db6a51e2837aa443cf48274468bb3fbb4
Clemente Gomez <zomegpro@gmail.com>
2016-02-10 10:03:10 -0500

    Fix Ports

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var bodyParser = require("body-parser");
 var app = express();
 var routes = require("./app/routes")(express.Router());
 var nymAPI = require("./app/wordAPI")(express.Router(), app);
+var host = process.env.NODE_ENV == "production" ? "0.0.0.0" : "localhost";
+var port = process.env.PORT || 5000;
 
 app.set("gameLevel", 2);
 
@@ -10,6 +12,6 @@ app.use(express.static(__dirname + "/public"));
 app.use("/api/nyms", nymAPI);
 app.use("/", routes);
 
-app.listen(0.0.0.0, function(){
+app.listen(port, host, function(){
 	console.log("Running");
 });
